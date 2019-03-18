fb7af033e3c33a8b7080d3ca8533960123c1add0
Pure-L0G1C <Ethical.Hacker720@gmail.com>
2019-03-17 20:12:11 -0400

    Update

--- static/js/session.js
+++ static/js/session.js
@@ -1,16 +1,31 @@
 'use strict';
 
+let mouseMoved = false;
 let isLoggedOut = false;
-const sessionCheckDelay = ((60 * 2) + 5) * 1000; // (Session's TTL + a few seconds) 2 minutes + 5 seconds
+let updatingSession = false;
+let lastUpdated = new Date();
+const sessionUpdateDelay = 5 * 1000; // 5 Seconds
+const sessionCheckDelay = (60 * 2 + 5) * 1000; // (Session's TTL + a few seconds) 2 minutes + 5 seconds
 
 $(document).ready(() => {
+    $(document).mousemove(() => {
+        mouseMoved = true;
+    });
+
     setInterval(sessionCheck, sessionCheckDelay);
+    setInterval(sessionUpdate, sessionUpdateDelay);
 });
 
 function sessionCheck() {
-    if (isLoggedOut) {
+    if (isLoggedOut || updatingSession) {
         return;
     }
+
+    if (new Date().getTime() - lastUpdated.getTime() <= sessionUpdateDelay) {
+        return;
+    }
+
+    updatingSession = true;
 
     $.ajax({
         type: 'POST',
@@ -18,9 +33,21 @@ function sessionCheck() {
     }).done(function(resp) {
         let respCode = resp['resp'];
 
+        mouseMoved = false;
+        updatingSession = false;
+        lastUpdated = new Date();
+
         if (respCode != 0) {
             isLoggedOut = true;
             window.location.href = '/';
-        } 
+        }
     });
+}
+
+function sessionUpdate() {
+    if (!mouseMoved || updatingSession) {
+        return;
+    }
+
+    sessionCheck();
 }
