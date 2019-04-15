c268dde99da1158a449060c13f53b58b485c91fe
Pure-L0G1C <Ethical.Hacker720@gmail.com>
2019-04-15 09:13:53 -0400

    Update

--- executable/static/js/topic.js
+++ executable/static/js/topic.js
@@ -12,7 +12,7 @@ function createnote() {
     let noteTitle = noteField.value;
 
     if (!noteTitle.trim() || isCreatingnote) {
-        return; 
+        return;
     }
 
     isCreatingnote = true;
@@ -20,22 +20,22 @@ function createnote() {
     $.ajax({
         type: 'POST',
         url: '/createnote',
-        data: { 'topic_id': topicId, 'note_title': noteTitle }
+        data: { topic_id: topicId, note_title: noteTitle }
     }).done(function(resp) {
         let respMsg = resp['resp'];
         let noteId = resp['note_id'];
         let dateCreated = resp['date_created'];
-        
+
         if (noteId) {
-            let $note = $('<div>', {'class': 'note'});
+            let $note = $('<div>', { class: 'note' });
             $note.attr('onclick', `location.href='/note?topic_id=${topicId}&note_id=${noteId}'`);
-    
-            let $noteTitle = $('<span>', {'class': 'note-name'});
-            let $noteDate = $('<span>', {'class': 'note-date'});
-    
+
+            let $noteTitle = $('<span>', { class: 'note-name' });
+            let $noteDate = $('<span>', { class: 'note-date' });
+
             $noteTitle.append(noteTitle);
             $noteDate.append(dateCreated);
-    
+
             $note.append($noteTitle);
             $note.append($noteDate);
             $('#notes').prepend($note);
@@ -44,15 +44,15 @@ function createnote() {
         if (noteField.classList.contains('error-msg')) {
             noteField.classList.remove('error-msg');
         }
-        
+
         if (noteField.classList.contains('success-msg')) {
             noteField.classList.remove('success-msg');
         }
-        
+
         if (respMsg == 'success-msg') {
             noteField.value = '';
         }
-        
+
         noteField.focus();
         isCreatingnote = false;
         noteField.classList.add(respMsg);
@@ -61,11 +61,11 @@ function createnote() {
 
 function getnotes() {
     let topicId = document.getElementById('topic-id').innerHTML;
-    
+
     $.ajax({
         type: 'POST',
         url: '/getnotes',
-        data: { 'topic_id': topicId }
+        data: { topic_id: topicId }
     }).done(function(resp) {
         let note;
         let noteId;
@@ -77,27 +77,26 @@ function getnotes() {
         let $noteDate;
         let $noteTitle;
 
-        for (let n=0; n<notes.length; n++) {
-
+        for (let n = 0; n < notes.length; n++) {
             note = notes[n];
             noteId = note['note_id'];
             noteTitle = note['note_title'];
             dateCreated = note['date_created'];
 
-            $note = $('<div>', {'class': 'note'});
+            $note = $('<div>', { class: 'note' });
 
             $note.attr('title', noteTitle);
             $note.attr('onclick', `location.href='/note?topic_id=${topicId}&note_id=${noteId}'`);
-                
-            $noteTitle = $('<span>', {'class': 'note-name'});
-            $noteDate = $('<span>', {'class': 'note-date'});
-    
+
+            $noteTitle = $('<span>', { class: 'note-name' });
+            $noteDate = $('<span>', { class: 'note-date' });
+
             $noteTitle.append(noteTitle);
             $noteDate.append(dateCreated);
-    
+
             $note.append($noteTitle);
             $note.append($noteDate);
-            $('#notes').append($note);         
+            $('#notes').append($note);
         }
     });
 }
